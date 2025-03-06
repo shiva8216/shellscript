a06e0219681075e09b0f6aac1a59f195ddb885ec
shiva <shiva.aws@gmail.com>
2025-03-06 16:12:23 +0530

    changes done

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1  # its arguments 
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: Hov are you"
+echo "$PERSON2:: i am good,vt about you?"
